dc6bf2c6423d722e33eb2a2b84002c2912c1264c
whirlwind17 <whirlwind13220@gmail.com>
2025-01-01 23:39:50 +0800

    myschool_1117

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv('DB_HOST') ?: 'localhost'; // 資料庫主機
-$db = getenv('DB_NAME') ?: 'game_db'; // 資料庫名稱
+$host = getenv('DB_HOST') ?: '13.54.4.168:8080'; // 資料庫主機
+$db = getenv('DB_NAME') ?: 'myschool1117'; // 資料庫名稱
 $user = getenv('DB_USER') ?: 'root'; // 使用者名稱
-$pass = getenv('DB_PASS') ?: ''; // 密碼
+$pass = getenv('DB_PASS') ?: 'root'; // 密碼
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
